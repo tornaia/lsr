7895da068d855d777ff4d16bd4a51615ecf195c5
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-09-05 19:40:32 +0200

    Add test case

--- lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveUp.java
+++ lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveUp.java
@@ -25,4 +25,20 @@ public class MoveUp extends AbstractTest {
 
         assertPathEqualsRecursively("scenarios/basic/move.up/expected", rootDirectory);
     }
+
+    @Test
+    public void moveTwoLevelsUp() {
+        File rootDirectory = createCopy("scenarios/basic/move.up.two.levels/before/");
+        File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
+
+        MavenProject mavenProject = new MavenProject(rootPom);
+        MavenCoordinate from = new MavenCoordinate("group-id", "child-child-artifact-id", "1.0");
+        MavenCoordinate parentTo = null;
+        MavenCoordinate as = new MavenCoordinate("group-id", "parent-artifact-id", "1.0");
+        MavenCoordinate what = new MavenCoordinate("org.apache.commons", "commons-lang3", "3.4");
+
+        new MoveDependency(mavenProject, from, as, parentTo, what).execute();
+
+        assertPathEqualsRecursively("scenarios/basic/move.up.two.levels/expected", rootDirectory);
+    }
 }
