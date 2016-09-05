8d75ac418fe41903fd735628b3bf10745dd0c623
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-09-05 20:16:48 +0200

    Add test case - move to new module

--- lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNext.java
+++ lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNext.java
@@ -43,6 +43,22 @@ public class MoveNext extends AbstractTest {
     }
 
     @Test
+    public void moveWithSourceDirectDependencyToNewModule() throws Exception {
+        File rootDirectory = createCopy("scenarios/basic/move.with.source.to.new/before/");
+        File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
+
+        MavenProject mavenProject = new MavenProject(rootPom);
+        MavenCoordinate from = new MavenCoordinate("group-id", "child-artifact-id-1", "1.0");
+        MavenCoordinate parentTo = new MavenCoordinate("group-id", "parent-artifact-id", "1.0");
+        MavenCoordinate as = new MavenCoordinate("group-id", "child-artifact-id-2", "1.0");
+        MavenCoordinate what = new MavenCoordinate("org.apache.commons", "commons-lang3", "3.4");
+
+        new MoveDependency(mavenProject, from, as, parentTo, what).execute();
+
+        assertPathEqualsRecursively("scenarios/basic/move.with.source.to.new/expected", rootDirectory);
+    }
+
+    @Test
     public void moveWithSourceTransitiveDependencyToExistingModule() throws Exception {
         File rootDirectory = createCopy("scenarios/basic/move.with.source.transitive.dependency/before/");
         File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
