46e7e45a48798581155f867a3ae09a2b48aec2d0
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-09-16 17:43:21 +0200

    Add test case One transitive and one direct dependency: a move transitive, b move direct

--- lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNextTest.java
+++ lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNextTest.java
@@ -79,8 +79,8 @@ public class MoveNextTest extends AbstractTest {
     }
 
     @Test
-    public void moveNextOneTransitiveOneDirectTransitiveIsMoving() throws Exception {
-        File rootDirectory = createCopy("scenarios/basic/move.next.one.transitive.one.direct.transitive.is.moving/before/");
+    public void moveNextOneTransitiveOneDirectWhenTransitiveIsMoving() throws Exception {
+        File rootDirectory = createCopy("scenarios/basic/move.next.one.transitive.one.direct.when.transitive.is.moving/before/");
         File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
 
         MavenProject mavenProject = new MavenProject(rootPom);
@@ -91,7 +91,23 @@ public class MoveNextTest extends AbstractTest {
 
         new MoveDependency(mavenProject, from, as, parentTo, what).execute();
 
-        assertPathEqualsRecursively("scenarios/basic/move.next.one.transitive.one.direct.transitive.is.moving/expected", rootDirectory);
+        assertPathEqualsRecursively("scenarios/basic/move.next.one.transitive.one.direct.when.transitive.is.moving/expected", rootDirectory);
+    }
+
+    @Test
+    public void moveNextOneTransitiveOneDirectWhenDirectIsMoving() throws Exception {
+        File rootDirectory = createCopy("scenarios/basic/move.next.one.transitive.one.direct.when.direct.is.moving/before/");
+        File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
+
+        MavenProject mavenProject = new MavenProject(rootPom);
+        MavenCoordinate from = new MavenCoordinate("group-id", "child-artifact-id-1", "1.0");
+        MavenCoordinate parentTo = new MavenCoordinate("group-id", "parent-artifact-id", "1.0");
+        MavenCoordinate as = new MavenCoordinate("group-id", "child-artifact-id-2", "1.0");
+        MavenCoordinate what = new MavenCoordinate("org.apache.commons", "commons-lang3", "3.4");
+
+        new MoveDependency(mavenProject, from, as, parentTo, what).execute();
+
+        assertPathEqualsRecursively("scenarios/basic/move.next.one.transitive.one.direct.when.direct.is.moving/expected", rootDirectory);
     }
 
     @Test
