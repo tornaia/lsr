713c327c1a70ce4d1897d2103d9b8eaf89962b60
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-09-16 17:29:07 +0200

    Add test case Two dependencies contain transitively the same dependency

--- lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNextTest.java
+++ lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNextTest.java
@@ -130,4 +130,20 @@ public class MoveNextTest extends AbstractTest {
 
         assertPathEqualsRecursively("scenarios/basic/move.next.when.dependency.already.there.with.different.version/before", rootDirectory);
     }
+
+    @Test
+    public void moveNextTwoTransitive() throws Exception {
+        File rootDirectory = createCopy("scenarios/basic/move.next.two.transitive/before/");
+        File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
+
+        MavenProject mavenProject = new MavenProject(rootPom);
+        MavenCoordinate from = new MavenCoordinate("group-id", "child-artifact-id-1", "1.0");
+        MavenCoordinate parentTo = new MavenCoordinate("group-id", "parent-artifact-id", "1.0");
+        MavenCoordinate as = new MavenCoordinate("group-id", "child-artifact-id-2", "1.0");
+        MavenCoordinate what = new MavenCoordinate("io.appium", "java-client", "4.1.2");
+
+        new MoveDependency(mavenProject, from, as, parentTo, what).execute();
+
+        assertPathEqualsRecursively("scenarios/basic/move.next.two.transitive/expected", rootDirectory);
+    }
 }
