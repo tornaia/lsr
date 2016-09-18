0012f08c26649d47ccf197c8b66183b8a1ecf1a7
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-09-18 08:28:37 +0200

    Add unit test Basic dependencyManagement tests: direct and inherited

--- lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNextTest.java
+++ lsr-core/src/test/java/com/github/tornaia/lsr/action/MoveNextTest.java
@@ -194,4 +194,36 @@ public class MoveNextTest extends AbstractTest {
 
         assertPathEqualsRecursively("scenarios/basic/move.next.transitive.inherited.direct.own.when.direct.is.moving/expected", rootDirectory);
     }
+
+    @Test
+    public void moveNextDependencyManagementDirect() throws Exception {
+        File rootDirectory = createCopy("scenarios/basic/move.next.dependency.management.direct/before/");
+        File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
+
+        MavenProject mavenProject = new MavenProject(rootPom);
+        MavenCoordinate from = new MavenCoordinate("group-id", "child-artifact-id-1", "1.0");
+        MavenCoordinate parentTo = new MavenCoordinate("group-id", "parent-artifact-id", "1.0");
+        MavenCoordinate as = new MavenCoordinate("group-id", "child-artifact-id-2", "1.0");
+        MavenCoordinate what = new MavenCoordinate("org.apache.commons", "commons-lang3", "3.4");
+
+        new MoveDependency(mavenProject, from, as, parentTo, what).execute();
+
+        assertPathEqualsRecursively("scenarios/basic/move.next.dependency.management.direct/expected", rootDirectory);
+    }
+
+    @Test
+    public void moveNextDependencyManagementInherited() throws Exception {
+        File rootDirectory = createCopy("scenarios/basic/move.next.dependency.management.inherited/before/");
+        File rootPom = new File(rootDirectory.getAbsolutePath() + File.separator + MavenModel.FILENAME_POM_XML);
+
+        MavenProject mavenProject = new MavenProject(rootPom);
+        MavenCoordinate from = new MavenCoordinate("group-id", "child-artifact-id-1", "1.0");
+        MavenCoordinate parentTo = new MavenCoordinate("group-id", "parent-artifact-id", "1.0");
+        MavenCoordinate as = new MavenCoordinate("group-id", "child-artifact-id-2", "1.0");
+        MavenCoordinate what = new MavenCoordinate("org.apache.commons", "commons-lang3", "3.4");
+
+        new MoveDependency(mavenProject, from, as, parentTo, what).execute();
+
+        assertPathEqualsRecursively("scenarios/basic/move.next.dependency.management.inherited/expected", rootDirectory);
+    }
 }
